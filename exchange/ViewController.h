6d646cc224c462bfd78e2f9a4d394726a38dd764
Tatsuya Umino <ber.m2.h@gmail.com>
2015-04-26 07:38:33 +0900

    Initial Commit

---
+++ exchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  exchange
+//
+//  Created by 海野 竜也 on 2015/04/26.
+//  Copyright (c) 2015年 海野 竜也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ exchange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  exchange
+//
+//  Created by 海野 竜也 on 2015/04/26.
+//  Copyright (c) 2015年 海野 竜也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
